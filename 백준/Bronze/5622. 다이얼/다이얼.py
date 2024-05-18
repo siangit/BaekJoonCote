c959459eb230d8aac0e9402c3e50ed70915d9098
SIANNI <164474193+siangit@users.noreply.github.com>
2024-05-18 23:11:37 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+x=input()
+answer=0
+for i in x:
+    i == str(i)
+    if i in 'ABC':
+        a=3
+    elif i in 'DEF':
+        a=4
+    elif i in 'GHI':
+        a=5
+    elif i in 'JKL':
+        a=6
+    elif i in 'MNO':
+        a=7
+    elif i in 'PQRS':
+        a=8
+    elif i in 'TUV':
+        a=9
+    elif i in 'WXYZ':
+        a=10
+    answer +=a
+print(answer)
