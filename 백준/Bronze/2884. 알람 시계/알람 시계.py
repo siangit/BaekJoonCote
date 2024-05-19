5264b017a64d769245c867c8db1c3eb23dab98ab
SIANNI <164474193+siangit@users.noreply.github.com>
2024-05-20 00:21:36 +0900

    [Bronze III] Title: 알람 시계, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,11 +1,18 @@
 H,M=map(int,input().split())
 
-if M < 45:
-    M = 60 - (45-M)
-    if H == 0:
-        H=24-1
-    else:
-        H=H-1
-else:
+
+if 45 <= M:
+    H == H
     M = M-45
+elif M < 45:
+    M = M+60-45
+    if H < 0:
+        H==abs(H)
+        H=24-H
+    elif H == 0:
+        H = 23
+    else:
+        H = H-1
+
+
 print(f'{H} {M}')
