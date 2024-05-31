14f34ad126b852af77c792908d6305778165f27a
SIANNI <164474193+siangit@users.noreply.github.com>
2024-05-31 21:09:04 +0900

    [Bronze III] Title: 팩토리얼, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -0,0 +1,11 @@
+import sys 
+sys.setrecursionlimit(10006)
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+n=int(input())
+print(factorial(n))
